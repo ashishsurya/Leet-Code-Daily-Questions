a94d2f7dec456819fee21dc4a4040203da527a20
Surya Ashish <ashishsurya126@gmail.com>
2022-06-13 20:27:38 +0530

    Update Triangle.cpp

--- Triangle.cpp
+++ Triangle.cpp
@@ -1,1 +1,17 @@
-// https://leetcode.com/problems/triangle/
+/*
+link : https://leetcode.com/problems/triangle/
+reference : https://www.youtube.com/watch?v=FdwDUkldcA0
+*/
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<int> helper(triangle.back());
+        for(int i=n-2;i>=0;i--) {
+            for(int j=0;j<triangle[i].size();j++) {
+                helper[j] = min(helper[j],helper[j+1]) + triangle[i][j];
+            }
+        }
+        return helper[0];
+    }
+};
